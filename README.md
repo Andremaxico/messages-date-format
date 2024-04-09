bea13e813cfe739a4cf446f7701c38c75c28b154
Solomko Andriy <andremaxico321@gmail.com>
2024-04-09 21:31:24 +0300

    fixed some errors

--- README.md
+++
@@ -1,2 +0,0 @@
-# messages-date-format
-Repo for transforming Date type to something like "13 of October 2017" or "yesterday"
---
+++ test/script.js
@@ -0,0 +1,4 @@
+const convert = require('messages-date-format');
+
+console.log('fkfk');
+console.log(convert((new Date()).getTime()));
